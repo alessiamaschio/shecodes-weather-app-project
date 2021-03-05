0298e640b70eb36eb6e0696888edfd1458562b69
Alessia <alessiamaschio@gmail.com>
2021-03-05 11:08:04 +0100

    Add formatData function, fix converted, add icons

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-let currentDay = new Date();
-let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+function formatDate(){
+  let currentDay = new Date();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-let weekDay = days[currentDay.getDay()];
-let hour = currentDay.getHours();
-let minute =
-  (currentDay.getMinutes() < 10 ? "0" : "") + currentDay.getMinutes();
-minute.toLocaleString(undefined, { minute: `2-digit` });
-let currentDate = document.querySelector(".current-date");
-currentDate.innerHTML = `${weekDay.toUpperCase()} ${hour}:${minute}`;
+  let weekDay = days[currentDay.getDay()];
+
+  let hour = (currentDay.getHours() < 10 ? "0" : "") + currentDay.getHours();
+  hour.toLocaleString(undefined, { hour: `2-digit` });
+
+  let minute = (currentDay.getMinutes() < 10 ? "0" : "") + currentDay.getMinutes();
+  minute.toLocaleString(undefined, { minute: `2-digit` });
+
+  return `${weekDay.toUpperCase()} ${hour}:${minute}`;
+}
+
+
 
 function handlePosition(position) {
   let lat = position.coords.latitude;
@@ -19,39 +25,36 @@ function handlePosition(position) {
   axios.get(url).then(displayCity);
   axios.get(url).then(displayTemperature);
   axios.get(url).then(displayMinMaxTemp);
-
 }
 
 function displayCity(response) {
- 
-    let city = response.data.name;
+  let city = response.data.name;
   cityUpdate.innerHTML = `${city}`;
-  let weatherDesc = response.data.weather[0].main;
+
+  let weatherDesc = response.data.weather[0].description;
   weatherDescription.innerHTML = `${weatherDesc}`;
+
   let iconCode = response.data.weather[0].icon;
-    let icon = weatherIcon.setAttribute(
+  let icon = weatherIcon.setAttribute(
       `src`,
       `https://openweathermap.org/img/wn/${iconCode}@2x.png`
     );
   weatherIcon.innerHTML = `${icon}`;
-  
-  
+
+  let currentDate = document.querySelector(".current-date");
+  currentDate.innerHTML = formatDate();
 }
 
 function displayTemperature(response) {
- 
   let currentTemp = Math.round(response.data.main.temp);
- 
-    return (temp.innerHTML = `${currentTemp}°`);
+  return (temp.innerHTML = `${currentTemp}°`);
   
 }
 
 function displayMinMaxTemp(response) {
-
     let minTemp = Math.round(response.data.main.temp_min);
     let maxTemp = Math.round(response.data.main.temp_max);
     return (minMaxTemp.innerHTML = `${minTemp}° / ${maxTemp}°`);
-  
 }
 
 
@@ -61,19 +64,15 @@ function searchCity(event) {
   let input = document.querySelector(".search");
   currentCity = input.value;
 
-
   if (currentCity.length === 0 || !isNaN(currentCity)) {
     alert(`Sorry! You need to enter a valid input :)`);
   } else {
       let apiKey = `bedfbe0fd1980c1b75bd73f4d5db9305`;
-  let units = `metric`;
-
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${apiKey}&units=${units}`;
-
-  axios.get(url).then(displayCity);
-   axios.get(url).then(displayTemperature);
-  axios.get(url).then(displayMinMaxTemp);
-
+      let units = `metric`;
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${apiKey}&units=${units}`;
+      axios.get(url).then(displayCity);
+      axios.get(url).then(displayTemperature);
+      axios.get(url).then(displayMinMaxTemp);
   }
 }
 
@@ -84,30 +83,18 @@ function searchCityEnter(event) {
     let input = document.querySelector(".search");
     currentCity = input.value;
 
-   
-
     if (currentCity.length === 0 || !isNaN(currentCity)) {
       alert(`Sorry! You need to enter a valid input :)`);
     } else {
        let apiKey = `bedfbe0fd1980c1b75bd73f4d5db9305`;
-    let units = `metric`;
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${apiKey}&units=${units}`;
-    axios.get(url).then(displayCity);
-    axios.get(url).then(displayTemperature);
-    axios.get(url).then(displayMinMaxTemp);
+        let units = `metric`;
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${apiKey}&units=${units}`;
+        axios.get(url).then(displayCity);
+        axios.get(url).then(displayTemperature);
+        axios.get(url).then(displayMinMaxTemp);
     }
   }
 }
-
-let currentCity = document.querySelector(".city");
-
-let cityUpdate = document.querySelector(".city-update");
-
-let searchButton = document.querySelector(".fa-search");
-let enterButton = document.querySelector(".search");
-
-searchButton.addEventListener("click", searchCity);
-enterButton.addEventListener("keydown", searchCityEnter);
 
 function fahrenheitConverter() {
   let apiKey = `bedfbe0fd1980c1b75bd73f4d5db9305`;
@@ -115,7 +102,10 @@ function fahrenheitConverter() {
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${apiKey}&units=${units}`;
   axios.get(url).then(displayTemperature);
   axios.get(url).then(displayMinMaxTemp);
-   
+
+    fahrenheitSymbol.classList.remove("hide");
+  fahrenheitSymbol.classList.add("show-unit");
+  celsiusSymbol.classList.add("hide");
   
 }
 
@@ -125,8 +115,9 @@ function celsiusConverter() {
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${apiKey}&units=${units}`;
   axios.get(url).then(displayTemperature);
   axios.get(url).then(displayMinMaxTemp);
-    
-  
+  fahrenheitSymbol.classList.add("hide");
+   celsiusSymbol.classList.remove("hide");
+  celsiusSymbol.classList.add("show-unit");
 }
 
 let fahrenheit = document.querySelector("#fahrenheit");
@@ -174,34 +165,24 @@ let settingsButton = document.querySelector(".settings-container");
 settingsButton.addEventListener("click", showMenu);
 
 function changeIcon(newIcon) {
-        let icon = document.querySelector(".copyright");
-        icon.innerHTML = newIcon;
-        changeIcon(`Coded with <i class="fas fa-bolt"></i>
+  let icon = document.querySelector(".copyright");
+  icon.innerHTML = newIcon;
+  changeIcon(`Coded with <i class="fas fa-bolt"></i>by
+  <a href="https://www.linkedin.com/in/alessia-maschio-a8317734/"
+  >Alessia</a>
+  &copy;2021`);
+}
 
-          by
-          <a href="https://www.linkedin.com/in/alessia-maschio-a8317734/"
-            >Alessia</a
-          >
-          &copy;2021`);
-      }
-
-      function resetIcon(newIcon) {
-        let icon = document.querySelector(".copyright");
-        icon.innerHTML = newIcon;
-        resetIcon(`Coded with <i class="fas fa-heart"></i>
-
-          by
-          <a href="https://www.linkedin.com/in/alessia-maschio-a8317734/"
-            >Alessia</a
-          >
-          &copy;2021`);
+function resetIcon(newIcon) {
+  let icon = document.querySelector(".copyright");
+  icon.innerHTML = newIcon;
+  resetIcon(`Coded with <i class="fas fa-heart"></i>by
+  <a href="https://www.linkedin.com/in/alessia-maschio-a8317734/"
+  >Alessia</a>
+  &copy;2021`);
 }
       
-     let loveIcon = document.querySelector(".copyright");
-      loveIcon.addEventListener("mouseenter", changeIcon);
-
-      let lightningIcon = document.querySelector(".copyright");
-lightningIcon.addEventListener("mouseleave", resetIcon);
+ 
       
 
 function getPastelColor() {
@@ -212,7 +193,6 @@ function getPastelColor() {
 
 function generateRandomGradient() {
   body.style.background = `linear-gradient(to top,${getPastelColor()},${getPastelColor()})`;
-
 }
 
 
@@ -220,3 +200,21 @@ let randomBackground = document.querySelector("#random-background-color");
 randomBackground.addEventListener("click", generateRandomGradient);
 
 let body = document.querySelector("body");
+let fahrenheitSymbol = document.querySelector("#fahrenheit-symbol");
+let celsiusSymbol = document.querySelector("#celsius-symbol");
+  
+let currentCity = document.querySelector(".city");
+
+let cityUpdate = document.querySelector(".city-update");
+
+let searchButton = document.querySelector(".fa-search");
+let enterButton = document.querySelector(".search");
+
+searchButton.addEventListener("click", searchCity);
+enterButton.addEventListener("keydown", searchCityEnter);
+
+let loveIcon = document.querySelector(".copyright");
+loveIcon.addEventListener("mouseenter", changeIcon);
+
+let lightningIcon = document.querySelector(".copyright");
+lightningIcon.addEventListener("mouseleave", resetIcon);
